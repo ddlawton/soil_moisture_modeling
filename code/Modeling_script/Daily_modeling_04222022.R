c180f80d2421ef040119b503285b0f5aa26a3143
ddlawton <ddlawton@asu.edu>
2022-04-29 10:43:01 -0400

    Final script for the paper.

--- code/Modeling_script/Daily_modeling_04222022.R
+++ code/Modeling_script/Daily_modeling_04222022.R
@@ -1,6 +1,6 @@
 #########
 # Averaging data
-#  to daily
+#  to daily means
 #  and filtering out just for drylands
 #  2022
 #########
@@ -9,10 +9,9 @@ rm(list=ls())
 
 #main modeling and diagnostic packages
 library(mgcv)
-library(glmmTMB)
 library(gratia)
-library(DHARMa)
-
+library(lme4)
+library(performance)
 
 #Supporting packages
 
@@ -20,12 +19,11 @@ library(tidyverse) # for all your data management and visualizing needs
 library(lubridate) # To help manage the date columns
 library(patchwork) # another ggplot helper package. allows to combined plots together
 library(ggpubr) # just some pleasing themes for ggplot
-library(viridis) # some nice colors
 library(data.table) # for the command 'fread' which is a faster version of read.csv
-library(ggridges)
-library(ggpubr)
-library(MetBrewer)
-library(emmeans)
+library(ggridges) #density plots
+library(MetBrewer) # some fun colors inspired by paintings in the met
+library(emmeans) #estimated marginal means
+
 # Functions
 
 stderr <- function(x, na.rm=FALSE) {
@@ -66,341 +64,210 @@ ridgelines
 
 ggplot(dat_2021_dry,aes(x=std,y=mean))+ geom_point(shape=21,alpha=0.8)  + geom_smooth(size=1.25,method="gam") 
 
-summary(dat_2021_dry)
 
 
-#Quick modeling
-
-
-
-mod_by_2021_dry_gauss <- bam(mean ~ te(DOY,by=Trt,bs="tp",k=100) +
-                               te(DOY,by=Block,bs="tp",k=100),
-                             select=TRUE,data=dat_2021_dry,family=scat(),
-                             discrete=TRUE,nthreads = 23)
-
-
-#playing with autocorrelation
+#GAMM modeling with a Continuous AR(1) Correlation Structure
 ctrl <- list(niterEM = 0, msVerbose = TRUE, optimMethod="L-BFGS-B")
 
-m0 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=50) +
-                                       te(DOY,Block,bs="fs",m=2,k=50),data=dat_2021_dry)
-
-
-m1 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=50) +
-                               te(DOY,Block,bs="fs",m=2,k=50),
-                               data=dat_2021_dry, correlation = corCAR1(value = 0.6,form = ~ DOY|block_trt),
-                               control = ctrl)
-
-
-k.check(m1$gam)
-
-m2 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-             te(DOY,Block,bs="fs",m=2,k=25),
-           data=dat_2021_dry, correlation = corARMA(p = 2,q=1,form = ~ DOY|block_trt),
+m0 <- gamm(mean ~ 
+             te(DOY,Trt,bs="fs",m=2,k=50) +
+             te(DOY,Block,bs="fs",m=2,k=50), select=TRUE,
+           data=dat_2021_dry,
            control = ctrl)
+acf(resid(m0$lme,type='normalized')) 
+pacf(resid(m0$lme,type='normalized'))
 
-m2 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-                                te(DOY,Block,bs="fs",m=2,k=25),
-                              data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 2),
-                              control = ctrl)
 
-m3 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-                                    te(DOY,Block,bs="fs",m=2,k=25),
-                                  data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 3),
-                                  control = ctrl)
-
-m4 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-                                    te(DOY,Block,bs="fs",m=2,k=25),
-                                  data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 4),
-                                  control = ctrl)
-
-m5 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-                                    te(DOY,Block,bs="fs",m=2,k=25),
-                                  data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 5),
-                                  control = ctrl)
-
-m6 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-             te(DOY,Block,bs="fs",m=2,k=25),
-           data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 6),
-           control = ctrl)
-
-m7 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-             te(DOY,Block,bs="fs",m=2,k=25),
-           data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 7),
-           control = ctrl)
-
-m7 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-             te(DOY,Block,bs="fs",m=2,k=25),
-           data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 7),
+m1 <- gamm(mean ~ 
+             te(DOY,Trt,bs= c('tp',"re"),m=2,k=50) +
+             te(DOY,Block,bs= c('tp',"re"),m=2,k=50), select=TRUE,
+           data=dat_2021_dry, correlation = corCAR1(value = 0.2,form = ~ DOY|block_trt),
            control = ctrl)
 
 
-m_est <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=25) +
-             te(DOY,Block,bs="fs",m=2,k=25),
-           data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY),
+acf(resid(m1$lme,type='normalized'))
+pacf(resid(m1$lme,type='normalized'))
+
+m2 <- gamm(mean ~ 
+             te(DOY,Trt,bs="fs",m=2,k=50) +
+             te(DOY,Block,bs="fs",m=2,k=50),
+           data=dat_2021_dry, correlation = corCAR1(value = 0.4,form = ~ DOY|block_trt), select=TRUE,
            control = ctrl)
+acf(resid(m2$lme,type='normalized')) 
+pacf(resid(m2$lme,type='normalized'))
 
-
-layout(matrix(1:2, ncol = 2))
-acf(resid(m0$lme, type = "normalized"))
-pacf(resid(m0$lme, type = "normalized"))
-layout(1)
-
-acf(resid(m1$lme, type = "normalized"))
-pacf(resid(m1$lme, type = "normalized"))
-
-acf(resid(m2$lme, type = "normalized"))
-pacf(resid(m2$lme, type = "normalized"))
-
-acf(resid(m3$lme, type = "normalized"))
-pacf(resid(m3$lme, type = "normalized"))
-
-acf(resid(m4$lme, type = "normalized"))
-pacf(resid(m4$lme, type = "normalized"))
-
-acf(resid(m5$lme, type = "normalized"))
-pacf(resid(m5$lme, type = "normalized"))
-
-acf(resid(m6$lme, type = "normalized"))
-pacf(resid(m6$lme, type = "normalized"))
-
-acf(resid(m7$lme, type = "normalized"))
-pacf(resid(m7$lme, type = "normalized"))
-layout(1)
-
-anova(m0$lme, m1$lme, m2$lme, m3$lme,m4$lme,m5$lme,m6$lme,m7$lme)
-
-
-
-?bam
-summary(mod_by_2021_dry_gauss)
-summary(m3$gam)
-
-k.check(mod_fs_2021_dry_gauss)
-
-AIC(m0$lme,m1$lme,m2$lme,m3$lme,m4$lme,m5$lme,m6$lme,m7$lme) %>%
-  mutate(deltaAIC = AIC - min(AIC)) %>%
-  arrange(deltaAIC)
-
-BIC(mod_by_2021_dry_gauss,mod_fs_2021_dry_gauss) %>%
-  mutate(deltaBIC = BIC - min(BIC)) %>%
-  arrange(deltaBIC)
-
-resids <- simulateResiduals(m1$lme)
-plot(m2$lme)
-(m3$gam)
-
-
-
-
-
-
-m0_est <- gratia::smooth_estimates(m0) %>%
-  mutate(mod = "m0",est = coef(m0$gam)[1] + est)
-m1_est <- gratia::smooth_estimates(m1)%>%
-  mutate(mod = "m1",est = coef(m1$gam)[1] + est)
-m2_est <- gratia::smooth_estimates(m2)%>%
-  mutate(mod = "m2",est = coef(m2$gam)[1] + est)
-m3_est <- gratia::smooth_estimates(m3)%>%
-  mutate(mod = "m3",est = coef(m3$gam)[1] + est)
-m4_est <- gratia::smooth_estimates(m4)%>%
-  mutate(mod = "m4",est = coef(m4$gam)[1] + est)
-m5_est <- gratia::smooth_estimates(m5)%>%
-  mutate(mod = "m5",est = coef(m5$gam)[1] + est)
-m6_est <- gratia::smooth_estimates(m6)%>%
-  mutate(mod = "m6",est = coef(m6$gam)[1] + est)
-m7_est <- gratia::smooth_estimates(m7)%>%
-  mutate(mod = "m7",est = coef(m7$gam)[1] + est)
-
-results <- m0_est %>% rbind(m1_est) %>%#,m2_est,m3_est,m4_est,m5_est,m6_est,m7_est) %>%
-  filter(smooth == "te(DOY,Trt)") %>%
-  select(est,se,DOY,Trt,mod)
-
-
-date_trt <- results %>% 
-  filter(mod %in% c("m0","m1")) %>%
-  ggplot(aes(x=DOY,group=mod)) + #geom_ribbon(aes(ymin=est-se, ymax=est+se,fill=mod),alpha=.1) +
-  geom_line(aes(y = est, color=mod),size=1.1) + theme_pubr() +
-  #geom_point(data=lm_dat,aes(x=DOY,y=mean,color=Trt)) +
-  #geom_smooth(data=lm_dat,aes(x=DOY,y=mean,color=Trt)) +
-  ggtitle("date by treatment smooth") + xlab("Date") +
-  facet_wrap(~Trt) +
-  scale_color_met_d(name="Demuth") +
-  scale_fill_met_d(name="Demuth")
-date_trt
-
-
-results <- m0_est %>% rbind(m1_est,m2_est,m3_est,m4_est,m5_est,m6_est,m7_est) %>%
-  filter(smooth == "te(DOY,Block)") %>%
-  select(est,se,DOY,Block,mod)
-
-date_block <- results %>% 
-  filter(mod %in% c("m0","m2")) %>%
-  ggplot(aes(x=DOY,group=mod)) + geom_ribbon(aes(ymin=est-se, ymax=est+se,fill=mod),alpha=.1) +
-  geom_line(aes(y = est, color=mod),size=1.1) + theme_pubr() + 
-  ggtitle("date by block smooth")+ xlab("Date") +
-  facet_wrap(~Block) +
-  scale_color_met_d(name="Demuth") +
-  scale_fill_met_d(name="Demuth")
-#date_block
-
-date_trt + date_block
-
-
-#Okay m2 seems to be the best of the evils here lets play around
-
-m0_round2 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=100) +
-             te(DOY,Block,bs="fs",m=2,k=100),data=dat_2021_dry)
-
-
-m3_round2 <- gamm(mean ~ te(DOY,Trt,bs="fs",m=2,k=100) +
-             te(DOY,Block,bs="fs",m=2,k=100),
-           data=dat_2021_dry, correlation = corARMA(form = ~ 1|DOY, p = 3),
+m3 <- gamm(mean ~ 
+             te(DOY,Trt,bs= c('gp',"re"),m=2,k=50) +
+             te(DOY,Block,bs="fs",m=2,k=50),
+           data=dat_2021_dry, correlation = corCAR1(value = 0.6,form = ~ DOY|block_trt),
            control = ctrl)
-
-summary(m0_round2$gam)
-summary(m3_round2$gam)
-
-
-m0_est_round2 <- gratia::smooth_estimates(m0_round2) %>%
-  mutate(mod = "m0",est = coef(m0_round2$gam)[1] + est)
-
-m3_est_round2 <- gratia::smooth_estimates(m3_round2)%>%
-  mutate(mod = "m3",est = coef(m2_round2$gam)[1] + est)
-
-results <- m0_est_round2 %>% rbind(m2_est_round2,m3_est_round2) %>%
-  filter(smooth == "te(DOY,Trt)") %>%
-  select(est,se,DOY,Trt,mod)
-
-
-date_trt <- results %>% 
-  filter(mod %in% c("m0","m2","m3")) %>%
-  ggplot(aes(x=DOY,group=mod)) + #geom_ribbon(aes(ymin=est-se, ymax=est+se,fill=mod),alpha=.1) +
-  geom_line(aes(y = est, color=mod),size=1.1) + theme_pubr() +
-  ggtitle("date by treatment smooth") + xlab("Date") +
-  facet_wrap(~Trt+mod,scales="free") +
-  scale_color_met_d(name="Demuth") +
-  scale_fill_met_d(name="Demuth")
-date_trt
-
-
-results <- m0_est_round2 %>% rbind(m2_est_round2) %>%
-  filter(smooth == "te(DOY,Block)") %>%
-  select(est,se,DOY,Block,mod)
-
-date_block <- results %>% 
-  filter(mod %in% c("m0","m2")) %>%
-  ggplot(aes(x=DOY,group=mod)) + geom_ribbon(aes(ymin=est-se, ymax=est+se,fill=mod),alpha=.1) +
-  geom_line(aes(y = est, color=mod),size=1.1) + theme_pubr() + 
-  ggtitle("date by block smooth")+ xlab("Date") +
-  facet_wrap(~Block) +
-  scale_color_met_d(name="Demuth") +
-  scale_fill_met_d(name="Demuth")
-#date_block
-
-date_trt + date_block
-
-#okay not sure about that, lets look at the linear shittt
-
-######https://bodowinter.com/tutorial/bw_LME_tutorial2.pdf
-
-test <- results %>% 
-  filter(mod %in% c("m0","m2"))
-
-         
-         
-         
-lm_dat %>%
-  group_by(DOY,Trt) %>%
-  summarize(mean = mean(mean), std=mean(std)) %>%
-  ggplot(aes(x=DOY,y=mean,color=Trt)) +
-  geom_point(size=3) +
-  geom_errorbar(aes(ymin=mean - std, ymax = mean + std),size=2) +
-  geom_line()
+acf(resid(m2$lme,type='normalized'))
+pacf(resid(m2$lme,type='normalized'))
 
 
 
-lm_dat <- dat_2021_dry %>%
-  mutate(doy_fact = factor(DOY))
+#alright there is still SOME temporal autocorrelation issues...but certainly better than not adding the correlation structure
 
-test <- lm_dat %>%
-  group_by(Block,doy_fact) %>%
-  summarize(count = length(doy_fact)) %>%
-  pivot_wider(names_from = "Block", values_from = count)
+anova(m3$lme,m0$lme, m1$lme, m2$lme)
 
-lm_dat$numfactor <- numFactor(lm_dat$DOY)
-
-parseNumLevels(levels(numFactor(lm_dat$DOY)))
-
-levels(lm_dat$doy_fact)
-?glmmTMB
-m1 <- glmmTMB(mean ~ Trt + DOY + Trt*DOY + (1|Block), data=dat_2021_dry,
-              control = glmmTMBControl(parallel = 10))
-
-m2 <- glmmTMB(mean ~ Trt + DOY + Trt*DOY + (1|Block) + corCAR1(value=0.6,form=~DOY|block_trt), data=lm_dat,
-              control = glmmTMBControl(parallel = 10))
-
-m3 <- glmmTMB(mean ~ Trt + DOY + Trt*DOY + (1|Block) + toep(numfactor + 0 | Block), data=lm_dat, dispformula=~0)
-
-m4 <- glmmTMB(mean ~ Trt + DOY + Trt*DOY + (1|Block) + ou(numfactor + 0 | Block),data=lm_dat,family=tweedie(),
-              control = glmmTMBControl(parallel = 10))
-
-AIC(m1,m2) %>%
+AIC(m3$lme,m0$lme, m1$lme, m2$lme) %>%
   mutate(delta = AIC - min(AIC)) %>%
   arrange(delta)
 
-BIC(m1,m2) %>%
+BIC(m3$lme,m0$lme, m1$lme, m2$lme) %>%
   mutate(delta = BIC - min(BIC)) %>%
   arrange(delta)
 
-
-plot(m2)
-
-data <- multcomp::cld(emmeans(m2, "Trt")) %>% as_tibble()
-
-data %>%
-  ggplot(aes(x=Trt,y=emmean)) +
-  geom_point() +
-  geom_errorbar(aes(ymin=emmean - SE,ymax = emmean +SE))
+appraise(m1$gam)
+summary(m1$gam)
+k.check(m1$gam) #Making sure that there are enough knots to capture all the wiggles (yes that the official term)
 
 
-
-summary(m1)
-summary(m2)
+#Now plotting to see results
 
 
+results_m0 <- gratia::smooth_estimates(m0)
 
-TukeyHSD(m2)
-
-performance::check_model(m1)
-resid <- DHARMa::simulateResiduals(m1)
-plot(resid)
-
-acf(resid(m1))
-acf(resid(m2))
-acf(resid(m4))
-
-pacf(resid(m1))
-pacf(resid(m2))
+results_m0$adj_est <- results_m0$est + coef(m0$gam)[1] #This is how I adjust to TWS units
 
 
+results_m1 <- gratia::smooth_estimates(m1)
 
-##model attempts
+results_m1$adj_est <- results_m1$est + coef(m1$gam)[1] #This is how I adjust to TWS units
 
-library("nlme")
-library("lme4")
+DOYxBlock_m0 <- results_m0 %>% # not sigificant in the model
+  filter(smooth == "te(DOY,Block)") %>%
+  ggplot(aes(x=DOY)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=Block),alpha=.2) +
+  geom_line(aes(y = adj_est,color=Block),size=1) + theme_pubr() +
+  ggtitle("Day of year by block smooth") + xlab("Day of year") +
+  scale_color_met_d(name="Demuth") +
+  scale_fill_met_d(name="Demuth")
 
-m1 <- lmer(mean ~ Trt + DOY + Trt*DOY + (1|Block), data=dat_2021_dry) #structure if random intercepts
+DOYxTrt_m0 <- results_m0 %>%
+  mutate(Trt = case_when(
+    Trt == "C" ~ "Cover",
+    Trt == "NC" ~ "Noncover")) %>%
+  filter(smooth == "te(DOY,Trt)") %>%
+  ggplot(aes(x=DOY,)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=Trt),alpha=.2) +
+  geom_line(aes(y = adj_est,color=Trt),size=1) + theme_pubr() +
+  xlab("Day of year") +
+  ylab("Modeled TWS") +
+  scale_color_met_d(name="Veronese") +
+  scale_fill_met_d(name="Veronese") +
+  theme(legend.title=element_blank())
 
-m2 <- lme(mean ~ Trt|DOY, data=dat_2021_dry, random = ~ 1|Block, correlation = corAR1(form = 1|Block), method="REML")
 
-m2b <- lme(mean ~ Trt|DOY, data=dat_2021_dry, random = ~ 1|Block, correlation = corAR1(), method="REML")
+DOYxBlock_m1 <- results_m1 %>% # not sigificant in the model
+  filter(smooth == "te(DOY,Block)") %>%
+  ggplot(aes(x=DOY)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=Block),alpha=.2) +
+  geom_line(aes(y = adj_est,color=Block),size=1) + theme_pubr() +
+  ggtitle("Day of year by block smooth") + xlab("Day of year") +
+  scale_color_met_d(name="Demuth") +
+  scale_fill_met_d(name="Demuth")
 
-m3 <- lme(mean ~ Trt + DOY + DOY*Trt, data=dat_2021_dry, random = ~ DOY|Block)
+DOYxTrt_m1 <- results_m1 %>%
+  mutate(Trt = case_when(
+    Trt == "C" ~ "Cover",
+    Trt == "NC" ~ "Noncover")) %>%
+  filter(smooth == "te(DOY,Trt)") %>%
+  ggplot(aes(x=DOY,)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=Trt),alpha=.2) +
+  geom_line(aes(y = adj_est,color=Trt),size=1) + theme_pubr() +
+  xlab("Day of year") +
+  ylab("Modeled TWS") +
+  scale_color_met_d(name="Veronese") +
+  scale_fill_met_d(name="Veronese") +
+  theme(legend.title=element_blank())
 
 
 
-citation("lme4")
+
+#comparing a model with the correlational structure and without
+
+m0_results <- (DOYxBlock_m0) / (DOYxTrt_m0)
+
+m1_results <- (DOYxBlock_m1) / (DOYxTrt_m1) # I nean the models dont change THAT much....but whatever
 
 
+#Now the traditional approach
+
+#making the basic line plot showing DOY by treatment
+
+Trt<-dat_2021_dry %>%
+  group_by(Trt,DOY) %>%
+  summarize(se = stderr(mean),mean = mean(mean))
+summary(Trt)
+
+
+line_graph <- dat_2021_dry %>%
+  mutate(Trt = case_when(
+    Trt == "C" ~ "Cover",
+    Trt == "NC" ~ "Noncover")) %>%
+  group_by(Trt,DOY) %>%
+  summarize(se = stderr(mean),mean = mean(mean)) %>%
+  ggplot(aes(x=DOY)) +
+  geom_point(aes(y=mean,color=Trt),size=0.9) +
+  geom_linerange(aes(ymin=mean-se,ymax= mean+se,color=Trt),size=0.5,alpha=0.5)+
+  geom_line(aes(y = mean,x=DOY,color=Trt),size=0.25) +
+  scale_color_met_d(name="Veronese") +
+  xlab("Day of year") +
+  ylab("Averaged TWS") +
+  ggpubr::theme_pubr() +
+  theme(legend.position="none")
+
+
+dat_2021_dry %>% #When all averaged together....there really isnt a significant difference. The time component is important!
+  ggplot(aes(x=Trt,y=mean)) +
+  geom_boxplot() +
+  geom_jitter(pch=21,size=0.75) +
+  ggpubr::theme_pubr() +
+  ylab("TWS") +
+  xlab("Treatment")
+
+
+dat_2021_dry %>%   #It is pretty clear that there are certain DOYs where there is a separation of the two covers....
+  mutate(DOY = factor(DOY)) %>%
+  ggplot(aes(x=DOY,y=mean,color=Trt)) +
+  geom_boxplot() +
+  geom_jitter(pch=21,size=0.75) +
+  ggpubr::theme_pubr() +
+  ylab("TWS") +
+  xlab("Day of Year")  +
+  scale_color_met_d(name="Veronese") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+
+
+
+
+dat_2021_dry <- dat_2021_dry %>%
+  mutate(block_trt = factor(block_trt),
+         DOY_fact = factor(DOY))
+
+lme_simple_fit <- lme(mean~ Trt,random=~1 + DOY | block_trt,data=dat_2021_dry,correlation=corCAR1(value=0.2,form=~DOY|block_trt)) # This is the exact same correlational structure as in the GAMs
+
+performance::check_model(lme_simple_fit) #It could definitely be better....but I think it gets the point across.
+
+
+plot(lme_simple_fit)
+acf(resid(lme_simple_fit,type="normalized"))
+pacf(resid(lme_simple_fit,type="normalized"))
+
+summary(lme_simple_fit)
+
+MuMIn::r.squaredGLMM(lme_simple_fit) #just to get that R-square
+summary(m1$gam)$r.sq
+
+emeans <- emmeans(lme_simple_fit, pairwise ~ Trt)
+emeans_df <- emeans$emmeans %>% as.data.frame()
+
+LME_emmeans <- ggplot(emeans_df,aes(x=Trt,y=emmean)) + geom_point() +
+  geom_errorbar(aes(ymin = emmean - SE,ymax=emmean+SE)) +
+  scale_y_continuous(limits=c(0,NA)) +
+  theme_pubr() + ylab("Modeled TWS")  + 
+  scale_x_discrete(labels = c('Cover','Noncover')) +
+  xlab("")
+
+plots <- (LME_emmeans + plot_spacer()) / line_graph / DOYxTrt + 
+  plot_annotation(tag_levels = 'A') +  theme(legend.position = "bottom")
+
+ggsave(plots,file="output/Figure_1.png",height = 7.5,width=5,units="in",dpi=600)
 
